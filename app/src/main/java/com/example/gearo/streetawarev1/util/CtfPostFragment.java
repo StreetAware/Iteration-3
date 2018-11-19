9d586fc6fc2a8b16037788acf091b3838302f18f
StreetAware <115495208@umail.ucc.ie>
2018-11-19 12:36:42 +0000

    Iteration 2 - Completed transition to firebase, created login, created registration. Now focusing on CTF section

--- app/src/main/java/com/example/gearo/streetawarev1/util/CtfPostFragment.java
+++ app/src/main/java/com/example/gearo/streetawarev1/util/CtfPostFragment.java
@@ -1,24 +1,117 @@
 package com.example.gearo.streetawarev1.util;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.example.gearo.streetawarev1.R;
 
-public class CtfPostFragment extends Fragment {
+public class CtfPostFragment extends Fragment implements SelectPhotoDialog.OnPhotoSelectedListener {
 
-    private static final String TAG = "CtfPostFragment";
+    private static final String TAG = "CTFPostFragment";
+
+    @Override
+    public void getImagePath(Uri imagePath) {
+        Log.d(TAG, "getImagePath: setting the image to imageview");
+        UniversalImageLoader.setImage(imagePath.toString(), mPostImage);
+        //assign to global variable
+        mSelectedBitmap = null;
+        mSelectedUri = imagePath;
+    }
+
+    @Override
+    public void getImageBitmap(Bitmap bitmap) {
+        Log.d(TAG, "getImageBitmap: setting the image to imageview");
+        mPostImage.setImageBitmap(bitmap);
+        //assign to a global variable
+        mSelectedUri = null;
+        mSelectedBitmap = bitmap;
+    }
+
+
+    //widgets
+    private ImageView mPostImage;
+    private EditText mTitle, mDescription, mPrice, mCountry, mStateProvince, mCity, mContactEmail;
+    private Button mPost;
+    private ProgressBar mProgressBar;
+
+    //vars
+    private Bitmap mSelectedBitmap;
+    private Uri mSelectedUri;
+
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.frament_ctf_post, container,false);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_ctf_post, container, false);
+        mPostImage = view.findViewById(R.id.post_image);
+        mTitle = view.findViewById(R.id.input_title);
+        mDescription = view.findViewById(R.id.input_description);
+        mCountry = view.findViewById(R.id.input_country);
+        mStateProvince = view.findViewById(R.id.input_state_province);
+        mContactEmail = view.findViewById(R.id.input_email);
+        mPost = view.findViewById(R.id.btn_post);
+        mProgressBar = (ProgressBar) view.findViewById(R.id.progressBar);
 
+        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
+        init();
         return view;
     }
+
+    private void init(){
+
+        mPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: opening dialog to choose new photo");
+                SelectPhotoDialog dialog = new SelectPhotoDialog();
+                dialog.show(getFragmentManager(), getString(R.string.dialog_select_photo));
+                dialog.setTargetFragment(CtfPostFragment.this, 1);
+
+            }
+        });
+    }
+
+    private void resetFields(){
+        UniversalImageLoader.setImage("", mPostImage);
+        mTitle.setText("");
+        mDescription.setText("");
+        mCountry.setText("");
+        mStateProvince.setText("");
+        mContactEmail.setText("");
+    }
+
+    private void showProgressBar(){
+        mProgressBar.setVisibility(View.VISIBLE);
+
+    }
+
+    private void hideProgressBar(){
+        if(mProgressBar.getVisibility() == View.VISIBLE){
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    /**
+     * Return true if the @param is null
+     * @param string
+     * @return
+     */
+    private boolean isEmpty(String string){
+        return string.equals("");
+    }
+
 }
